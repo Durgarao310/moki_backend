9d6f51772c2e98f64d796a42121dbbeb28caed0b
Durgarao310 <gantadurgarao304@gmail.com>
2021-12-29 09:20:19 +0530

    second day commot

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,25 +1,29 @@
-import mongoose, { Schema, Document } from "mongoose"
+import mongoose, { Schema, Document } from 'mongoose'
 
 export interface IUser extends Document {
-  email: String;
-  firstName: String;
-  lastName: String;
+  email: String
+  userName: String
+  profilePic: string
+  password: String
+  mobileNumber: String
 }
 
 const UserSchema: Schema = new Schema({
   email: {
     type: String,
     required: true,
+    unique: true
+  },
+
+  userName: {
+    type: String,
+    required: true
+  },
+  mobileNumber: {
+    type: String,
     unique: true,
-  },
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
+    minlength: [10, 'Must be at least 10 numbers for mobile number']
+  }
 })
 
-export default mongoose.model < IUser > ("User", UserSchema)
+export default mongoose.model<IUser>('User', UserSchema)
